1495eead913f035915a6b93a1a62b92b3fb4efde
XuerCai <56418954+XuerCai@users.noreply.github.com>
2020-10-20 18:01:18 +0800

    Delete test file

--- sso-server2/src/main/java/com/cloud/sso/server/JVMCache.java
+++
@@ -1,9 +0,0 @@
-package com.cloud.sso.server;
-
-import java.util.HashMap;
-import java.util.Map;
-
-public class JVMCache {
-
-    public static Map<String, String> TICKET_AND_NAME = new HashMap<String, String>();
-}
